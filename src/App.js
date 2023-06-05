6c977defa916fe010963daf50f3d342cea77d4bb
MahendaranMahi <mahendaran.elangovan@immo.capital>
2023-06-05 18:54:54 +0530

    Cleanup

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
-import "./App.css";
 import { CreateJobFormOne } from "./pages/CreateJobFormOne";
 import { CreateJobFormTwo } from "./pages/CreateJobFormTwo";
 import { JobDescList } from "./pages/JobDescList";
 
 function App() {
   return (
-    <>
-      <div className="flex flex-col items-center gap-10 p-2.5">
-        <CreateJobFormOne />
-        <CreateJobFormTwo />
-        <JobDescList />
-      </div>
-    </>
+    <div className="flex flex-col items-center gap-10 p-2.5">
+      <CreateJobFormOne />
+      <CreateJobFormTwo />
+      <JobDescList />
+    </div>
   );
 }
 
